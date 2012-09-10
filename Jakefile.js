08a0db1fb9c5028e4adcb9b7a50d3da8f13348ee
Irving Cordova <irvingc@Irving-Cordovas-MacBook-Pro-2.local>
2012-09-09 17:52:18 -0700

    Empty grammar for scheme

--- Jakefile.js
+++ Jakefile.js
@@ -1,28 +1,44 @@
 var fs = require('fs');
-var TARGET_DIR = 'lib';
-var SRC_DIR = 'grammars';
+var path = require('path');
+var TARGET_DIR = 'bin';
+var SRC_DIR = 'grammar';
+var GRAMMAR_EXTENSION = '.par';
+
+function endsWith(str, suffix) {
+  return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
 
 desc('Compile the Scheme grammar with JS/CC');
 task('default', function(params) {
 
     jake.mkdirP(TARGET_DIR);
 
-    var grammars = jake.readdirR('grammars');
+    var grammars = jake.readdirR(SRC_DIR);
+    
+    var cmds = [];
 
     grammars.forEach(function(grammar) {
-      if(fs.statSync(grammar).isFile()) {
-        var output = grammar.replace(SRC_DIR, '');
-        output = TARGET_DIR + output.replace('.par', '.js');
-        console.log("Compiling: " + grammar + " to: " + output);
-        jake.exec(['jscc -v -w -o ' + output +' ' + grammar], {printStdout: true});
+      if(fs.statSync(grammar).isFile() 
+        && endsWith(grammar, GRAMMAR_EXTENSION)) {
+        var output = TARGET_DIR + "/" +  path.basename(grammar, GRAMMAR_EXTENSION) + '.js';
+        cmds.push('jscc -v -w -o ' + output +' ' + grammar);
+        console.log("Preparing to compile: " + grammar + " to: " + output);
       }
     });
-});
+
+    jake.exec(cmds, function () {
+      console.log('Grammar compilation finished');
+      complete();
+    }, {printStdout: true});
+
+}, {async: true} );
 
 desc('Run the tests for the Scheme grammar');
 task('test', ['default'], function(params) {
-    jake.exec(['mocha'], {printStdout: true, printStderr: true});
-});
+    jake.exec(['mocha'], function() {
+      complete();
+    },{printStdout: true, printStderr: true});
+}, {async: true});
 
 desc('Cleans all files');
 task('clean', function(params) {
